48068c4cbe1dae5271bf01b20e0a3efa0c4f3405
unknown <mindc1july220@iiht.tech>
2022-09-24 11:40:57 +0530

    first commit

---
+++ Rahul.Bhasagi@mindtree.com/microservice-doctor/src/main/java/com/orchard/doctor/controller/DoctorController.java
@@ -0,0 +1,53 @@
+package com.orchard.doctor.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.orchard.doctor.entity.Doctor;
+import com.orchard.doctor.repository.DoctorRepository;
+import com.orchard.doctor.services.DoctorServiceImpl;
+
+@RestController
+@RequestMapping("/doctor")
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
+public class DoctorController {
+	
+	@Autowired
+	private DoctorServiceImpl doctorServiceImpl;
+	
+	@GetMapping("/")
+	public ResponseEntity<List<Doctor>> getAllDoctor(){
+		return new ResponseEntity<List<Doctor>>(doctorServiceImpl.getAllDoctor(), HttpStatus.OK);
+	}
+	
+	
+	@PostMapping("/")
+	public ResponseEntity<Doctor> saveDoctor(@RequestBody Doctor doctor){
+		return new ResponseEntity<Doctor>(doctorServiceImpl.saveDoctor(doctor), HttpStatus.OK);
+	}
+	
+
+	
+	
+	@GetMapping("/{doctorId}")
+	public ResponseEntity<Doctor> findByDoctorId(@PathVariable("doctorId") Long doctorId) {
+		return new ResponseEntity<Doctor>(doctorServiceImpl.findByDoctorId(doctorId), HttpStatus.OK);
+	}
+	
+	
+}
+
+
+
+
+
+
